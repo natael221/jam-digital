f6c710726c491e1dc0037a1aa93d7f6eb5392a9b
Natanael S <96678889+natael221@users.noreply.github.com>
2024-10-29 01:39:07 +0700

    Update api.js

--- api.js
+++ api.js
@@ -1,26 +1,19 @@
 const express = require('express');
 const fs = require('fs');
-const path = require('path');
-
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-// Fungsi untuk mengambil quotes secara acak
-function getRandomQuote() {
-    const quotesPath = path.join(__dirname, 'quotes.json');
-    const quotes = JSON.parse(fs.readFileSync(quotesPath, 'utf8'));
+const getRandomQuote = () => {
+    const quotes = JSON.parse(fs.readFileSync('quotes.json'));
     const randomIndex = Math.floor(Math.random() * quotes.length);
     return quotes[randomIndex];
-}
+};
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
-
-// Endpoint untuk mendapatkan quote secara acak
 app.get('/quote', (req, res) => {
     const quote = getRandomQuote();
     res.json({ quote });
 });
 
-module.exports = app; // Ekspor aplikasi untuk Vercel
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
